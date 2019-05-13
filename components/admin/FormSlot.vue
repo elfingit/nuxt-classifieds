d67853ffc911ac0d77166231d63b819b29872622
Andrey Sergeev <andreylong@gmail.com>
2019-05-13 13:00:51 +0300

    categories on admin dashboard - started

---
+++ components/admin/FormSlot.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="form-slot">
+    <div class="form-slot-container">
+      <div class="card">
+        <div class="card-header">
+          <slot name="header"></slot>
+        </div>
+        <div class="card-body">
+          <slot name="main"></slot>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="cover"></div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "FormSlot",
+
+    methods: {
+      show() {
+        this.$el.style.display = 'block'
+
+        let position = 100
+
+        let timerId = setInterval(() => {
+
+          this.$el.querySelector('.form-slot-container').style.left = position + '%'
+
+          position -= 10
+
+        }, 25)
+
+        setTimeout(() => {
+          clearInterval(timerId)
+        }, 180)
+
+        this.$el.querySelector('.cover').style.display = 'block'
+      },
+
+      hide() {
+        this.$el.style.display = 'none'
+        this.$el.querySelector('.form-slot-container').style.left = '-150%'
+        this.$el.querySelector('.cover').style.display = 'none'
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+  .form-slot {
+    display: none;
+    position: fixed;
+    width: 100%;
+  }
+  .form-slot-container {
+    position: relative;
+    width: 50%;
+    left: -150%;
+    z-index: 105;
+  }
+
+  .cover {
+    position: fixed;
+    top:0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(100, 100, 100, 0.5);
+    display: none;
+    z-index: 100;
+  }
+
+</style>
--- pages/admin/categories.vue
+++ pages/admin/categories.vue
@@ -1,14 +1,59 @@
-<template>
-<div class="row">
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+  <div class="col-lg-12">
+    <form-slot>
+      <template v-slot:header>Add Category</template>
+      <template v-slot:main>
+        <form action="" method="post">
+          <div class="form-group">
+            <label>Name</label>
+            <input type="text" name="name" class="form-control">
+          </div>
+          <div class="form-group">
+            <label>Parent category</label>
+            <select name="parent" class="form-control">
 
-</div>
+            </select>
+          </div>
+          <div class="form-group">
+            <button type="submit" class="btn btn-primary">Save</button>
+            <button type="reset" class="btn btn-info" v-on:click="hideForm()">Cancel</button>
+          </div>
+        </form>
+      </template>
+    </form-slot>
+    <button class="btn btn-primary" v-on:click="addCategory()">Add category</button>
+    <table class="table table-bordered">
+      <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Created At</th>
+        <th scope="col">Updated At</th>
+      </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "categories",
-        layout: 'admin'
+
+  import FormSlot from "~/components/admin/FormSlot"
+
+  export default {
+      name: "categories",
+    components: {FormSlot},
+    layout: 'admin',
+
+    methods: {
+        addCategory() {
+          this.$children[0].show()
+        },
+        hideForm() {
+          this.$children[0].hide()
+        }
     }
+  }
 </script>
 
 <style scoped>
