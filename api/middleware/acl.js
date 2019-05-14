11037a3b8d332f83092f4a7197c7779bad5a00b2
Андрей Сергеев <andreylong@gmail.com>
2019-05-14 06:09:35 +0300

    acl middleware - refactoring

--- api/middleware/acl.js
+++ api/middleware/acl.js
@@ -1,41 +1,43 @@
 
-const acl = (req, res, next) => {
+const acl = (role) => {
+  return (req, res, next) => {
 
-  const { verify_token } = require('../lib/token')
-  const userModel = require('../models/User')
+    const { verify_token } = require('../lib/token')
+    const userModel = require('../models/User')
 
-  new Promise((resolve, reject) => {
+    new Promise((resolve, reject) => {
 
-    if (!req.signedCookies || !req.signedCookies.token) {
-      return reject(new Error('Not token in cookies'))
-    }
+      if (!req.signedCookies || !req.signedCookies.token) {
+        return reject(new Error('Not token in cookies'))
+      }
 
-    const payload = verify_token(req.signedCookies.token)
+      const payload = verify_token(req.signedCookies.token)
 
-    if (!payload) {
-      return reject(new Error('Payload is empty'))
-    }
+      if (!payload) {
+        return reject(new Error('Payload is empty'))
+      }
 
-    userModel.byId(payload.id).then((u) => {
-      u.role().then((r) => {
-        if (r.get('name').toLowerCase() === 'admin') {
-          return resolve()
-        } else {
-          return reject(new Error('Bad user role'))
-        }
+      userModel.byId(payload.id).then((u) => {
+        u.role().then((r) => {
+          if (r.get('name').toLowerCase() === role) {
+            return resolve()
+          } else {
+            return reject(new Error('Bad user role'))
+          }
+        }).catch((e) => {
+          return reject(e)
+        })
       }).catch((e) => {
         return reject(e)
       })
+    }).then(() => {
+      return next()
     }).catch((e) => {
-      return reject(e)
+      console.error(e)
+      return res.status(403).end()
     })
-  }).then(() => {
-    return next()
-  }).catch((e) => {
-    console.error(e)
-    return res.status(403).end()
-  })
 
+  }
 }
 
 module.exports = acl
