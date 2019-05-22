e92c296f35577aac7250cb5f39407bbdda9b96c2
Andrey Sergeev <andreylong@gmail.com>
2019-05-22 20:50:53 +0300

    form popup - fixes

--- components/admin/FormSlot.vue
+++ components/admin/FormSlot.vue
@@ -1,19 +1,17 @@
 <template>
-  <div class="form-slot">
-    <div class="form-slot-container">
-      <div class="card">
-        <div class="card-header">
-          <slot name="header"></slot>
-        </div>
-        <div class="card-body">
-          <slot name="main"></slot>
+  <div class="cover">
+    <div class="form-slot">
+      <div class="form-slot-container">
+        <div class="card">
+          <div class="card-header">
+            <slot name="header"></slot>
+          </div>
+          <div class="card-body">
+            <slot name="main"></slot>
+          </div>
         </div>
       </div>
-
     </div>
-
-    <div class="cover"></div>
-
   </div>
 </template>
 
@@ -39,13 +37,13 @@
           clearInterval(timerId)
         }, 300)
 
-        this.$el.querySelector('.cover').style.display = 'block'
+        this.$el.style.display = 'block'
       },
 
       hide() {
         this.$el.style.display = 'none'
         this.$el.querySelector('.form-slot-container').style.left = '-150%'
-        this.$el.querySelector('.cover').style.display = 'none'
+        this.$el.style.display = 'none'
       }
     }
 
@@ -54,9 +52,10 @@
 
 <style scoped>
   .form-slot {
-    display: none;
-    position: fixed;
+    display: block;
+    position: relative;
     width: 100%;
+    margin-top: 1rem;
   }
   .form-slot-container {
     position: relative;
@@ -73,7 +72,7 @@
     height: 100%;
     background: rgba(100, 100, 100, 0.5);
     display: none;
-    z-index: 100;
+    z-index: 99999997;
   }
 
 </style>
